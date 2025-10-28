7adea9f917fcc9d0a7e96d0018fdfd3af2615846
irfan <peatergripin@gmail.com>
2025-10-29 06:59:47 +0800

    complete lesson 7 ex4

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.Parent}>
+      <View style={[styles.Child, { backgroundColor: "lightblue" }]}>
+        <Text>Square 1</Text>
+      </View>
+      <View style={[styles.Child, { backgroundColor: "lightgreen" }]}>
+        <Text>Square 2</Text>
+      </View>
+      <View style={[styles.Child, { backgroundColor: "coral" }]}>
+        <Text>Square 3</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Parent: {
+    backgroundColor: "whitesmoke",
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-around",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  Child: {
+    width: 80,
+    height: 80,
   },
 });
